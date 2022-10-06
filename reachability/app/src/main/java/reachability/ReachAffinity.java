70c84e39faed4d938a5535737b948fb716f2c070
thesave <Thesave86@gmail.com>
2022-10-06 21:36:13 +0200

    show all possible solutions

--- reachability/app/src/main/java/reachability/ReachAffinity.java
+++ reachability/app/src/main/java/reachability/ReachAffinity.java
@@ -28,87 +28,89 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+
 public class ReachAffinity {
-	public static void main( String[] args ) throws IOException {
-		if ( args.length < 1 ){
-			System.out.println( "Provide a path to a nodelist file as first argument");
-			System.exit( 1 );
-		}
-		if ( args.length < 2 ){
-			System.out.println( "Provide the capacity of the target worker as second argument");
-			System.exit( 1 );
-		}
-		String nodeList = Files.readString( Path.of( args[ 0 ] ) );
-		int capacity = Integer.parseInt( args[ 1 ] );
-		Graph g = Graph.parse( nodeList );
-		if ( args.length > 2 && args[ 2 ].equals( "--debug" ) ){
-			System.out.println( "Parsed: " + g );
-		}
-		canReach( g, capacity, true );
-		System.exit( 0 );
-	}
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Provide a path to a nodelist file as first argument");
+            System.exit(1);
+        }
+        if (args.length < 2) {
+            System.out.println("Provide the capacity of the target worker as second argument");
+            System.exit(1);
+        }
+        String nodeList = Files.readString(Path.of(args[0]));
+        int capacity = Integer.parseInt(args[1]);
+        Graph g = Graph.parse(nodeList);
+        if (args.length > 2 && args[2].equals("--debug")) {
+            System.out.println("Parsed: " + g);
+        }
+        canReach(g, capacity, true);
+        System.exit(0);
+    }
 
-	static boolean canReach( Graph g, int k, boolean topCall ) {
-		int total_occupation = g.root + g.children.stream().reduce( 0, ( a, c ) -> a + c.root, Integer::sum );
-		if ( total_occupation > k )
-			return false;
+    static boolean canReach(Graph g, int k, boolean topCall) {
+        int total_occupation = g.root + g.children.stream().reduce(0, (a, c) -> a + c.root, Integer::sum);
+        if (total_occupation > k)
+            return false;
 
-		if ( g.children.isEmpty() )
-			return true;
+        if (g.children.isEmpty())
+            return true;
 
-		Set< Graph[] > permutations = getPermutations( g.children.size(), g.children.toArray( new Graph[ 0 ] ) );
+        Set<Graph[]> permutations = getPermutations(g.children.size(), g.children.toArray(new Graph[0]));
 
-		for ( Graph[] permutation : permutations ){
-			if( checkPermutation( permutation, 0, k ) ) {
-				if( topCall )
-					System.out.println( "Found possible solution: " + Arrays.toString( permutation ) );
-				return true;
-			}
-		}
-		return false;
-	}
+        for (Graph[] permutation : permutations) {
+            if (checkPermutation(permutation, 0, k)) {
+                if (topCall) {
+                    System.out.println("Found possible solution: " + Arrays.toString(permutation));
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	static boolean canReach( Graph g, int k ){
-		return canReach( g, k, false );
-	}
+    static boolean canReach(Graph g, int k) {
+        return canReach(g, k, false);
+    }
 
-	static boolean checkPermutation( Graph[] g, int i, int k ){
-		if ( g.length > i && canReach( g[ i ], k ) ) {
+    static boolean checkPermutation(Graph[] g, int i, int k) {
+        if (g.length > i && canReach(g[i], k)) {
 //			System.out.println( "OK permutation " + Arrays.toString( g ) + " i: " + i + " k: " + k );
-			if( g.length > i+1 ){
-				return checkPermutation( g, i+1, k - g[ i ].root );
-			} else {
-				return true;
-			}
-		}
-		else {
+            if (g.length > i + 1) {
+                return checkPermutation(g, i + 1, k - g[i].root);
+            } else {
+                return true;
+            }
+        } else {
 //			System.out.println( "Wrong permutation " + Arrays.toString( g ) + " i: " + i + " k: " + k );
 //			System.out.println( "g.length: " + g.length + ", i:" + i +
 //							" canReach:" + ( g.length > i ? canReach( g[ i ], k ) : "false" ) );
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 
-	public static < T >  Set< T[] > getPermutations( int n, T[] a ) {
-		T tmp;
-		Set< T[] > s = new HashSet<>();
-		if ( n == 1 ) {
-			s.add( Arrays.copyOf( a, a.length ) );
-		} else {
-			for ( int i = 0; i < ( n - 1 ); i++ ) {
-				s.addAll( getPermutations( n - 1, a ) );
-				if ( n % 2 == 0 ) {
-					tmp = a[ i ];
-					a[ i ] = a[ n - 1 ];
-				} else {
-					tmp = a[ 0 ];
-					a[ 0 ] = a[ n - 1 ];
-				}
-				a[ n - 1 ] = tmp;
-			}
-			s.addAll( getPermutations( n - 1, a ) );
-		}
-		return s;
-	}
+    public static <T> Set<T[]> getPermutations(int n, T[] a) {
+        T tmp;
+        Set<T[]> s = new HashSet<>();
+        if (n == 1) {
+            s.add(Arrays.copyOf(a, a.length));
+        } else {
+            for (int i = 0; i < (n - 1); i++) {
+                s.addAll(getPermutations(n - 1, a));
+                if (n % 2 == 0) {
+                    tmp = a[i];
+                    a[i] = a[n - 1];
+                } else {
+                    tmp = a[0];
+                    a[0] = a[n - 1];
+                }
+                a[n - 1] = tmp;
+            }
+            s.addAll(getPermutations(n - 1, a));
+        }
+        return s;
+    }
 
 }
